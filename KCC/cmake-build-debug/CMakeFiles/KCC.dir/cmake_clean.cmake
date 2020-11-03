2ff3132bee0c75082a50f9fe8406aa3bc91e7199
J-Rind <68114555+J-Rind@users.noreply.github.com>
2020-11-02 19:04:55 -0600

    Intial upload

---
+++ KCC/cmake-build-debug/CMakeFiles/KCC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KCC.dir/main.c.o"
+  "KCC"
+  "KCC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/KCC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
